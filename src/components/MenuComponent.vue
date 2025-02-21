ce6e61347946a5f50b1d4ed75f2a4d2f03fb44c4
Matheus <matheus_benini@hotmail.com>
2025-02-21 11:35:34 -0300

    feat: add menu component

---
+++ src/components/MenuComponent.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { ref } from "vue";
+
+const isMenuOpen = ref(false);
+
+const toggleMenu = () => {
+  isMenuOpen.value = !isMenuOpen.value;
+};
+</script>
+
+<template>
+  <div>
+    <div class="menu-icon" @click="toggleMenu">
+      <span v-if="!isMenuOpen">&#9776;</span>
+    </div>
+
+    <div :class="['menu', { 'menu-open': isMenuOpen }]">
+      <div class="menu-header">
+        <span>Menu</span>
+        <span v-if="isMenuOpen" class="close-icon" @click="toggleMenu"
+          >&#10006;</span
+        >
+      </div>
+      <div class="menu-content"></div>
+    </div>
+  </div>
+</template>
+
+<style lang="less" src="@/styles/menu.less"></style>
