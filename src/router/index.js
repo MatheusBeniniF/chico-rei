8b4ba62eaaa58deed663bf0923cc35ddac3ebf7b
Matheus <matheus_benini@hotmail.com>
2025-02-23 22:54:43 -0300

    feat: add desafio extra

--- src/router/index.js
+++ src/router/index.js
@@ -4,12 +4,14 @@ import CheckoutView from "@/views/CheckoutView.vue";
 import HomeView from "@/views/HomeView.vue";
 import CamisetasClaras from "@/views/CamisetasClaras.vue";
 import CamisetasEscuras from "@/views/CamisetasEscuras.vue";
+import DesafioExtra from "@/views/DesafioExtra.vue";
 
 const routes = [
   { path: "/", component: HomeView },
   { path: "/checkout", component: CheckoutView },
   { path: "/camisetas-claras", component: CamisetasClaras },
   { path: "/camisetas-escuras", component: CamisetasEscuras },
+  { path: "/desafio-14", component: DesafioExtra },
 ];
 
 const router = createRouter({
---
+++ src/views/DesafioExtra.vue
@@ -0,0 +1,76 @@
+<script setup>
+import App from "@/assets/app.png";
+import ChatIcon from "@/svgs/chat.svg";
+import GroupIcon from "@/svgs/group.svg";
+import BrowserIcon from "@/svgs/browser.svg";
+import DesignIcon from "@/svgs/design.svg";
+import PhoneIcon from "@/svgs/phone.svg";
+import RetinaIcon from "@/svgs/retina.svg";
+</script>
+<template>
+  <div class="container">
+    <div class="content">
+      <h4>APP FEATURES</h4>
+      <h7>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Viverra nunc
+        ante velit <br />
+        vitae. Est tellus vitae, nullam lobortis enim. Faucibus amet etiam
+        tincidunt rhoncus, <br />
+        ullamcorper velit. Ullamcorper risus tempor, ac nunc libero urna,
+        feugiat.
+      </h7>
+    </div>
+
+    <div class="content top-info">
+      <img :src="ChatIcon" alt="Ícone de Chat" class="svg" />
+      <h5>Full free chat</h5>
+      <h7>
+        Lorem ipsum dolor sit amet, consectetur <br />
+        adipiscing elit.
+      </h7>
+    </div>
+    <div class="content-img">
+      <div class="content-side">
+        <div>
+          <div>
+            <img :src="BrowserIcon" alt="Ícone de Navegador" class="svg" />
+          </div>
+          <h5>unlimiter features</h5>
+          <h7>Lorem ipsum dolor sit amet, consectetur <br> adipiscing elit.</h7>
+        </div>
+        <div>
+          <div>
+            <img :src="DesignIcon" alt="Ícone de Design" class="svg" />
+          </div>
+          <h5>awsome ui design</h5>
+          <h7>Lorem ipsum dolor sit amet, consectetur <br> adipiscing elit.</h7>
+        </div>
+      </div>
+
+      <img class="app" :src="App" alt="Imagem de fundo" />
+
+      <div class="content-side">
+        <div>
+          <img :src="PhoneIcon" alt="Ícone de Telefone" class="svg" />
+          <h5>iso & androind version</h5>
+          <h7>Lorem ipsum dolor sit amet, consectetur <br> adipiscing elit.</h7>
+        </div>
+        <div>
+          <img :src="RetinaIcon" alt="Ícone de Retina" class="svg" />
+          <h5>retina ready greaphics</h5>
+          <h7>Lorem ipsum dolor sit amet, consectetur <br> adipiscing elit.</h7>
+        </div>
+      </div>
+    </div>
+
+    <div class="content">
+      <img :src="GroupIcon" alt="Ícone de Grupo" class="svg" />
+      <h4>24/7 support by real pepole</h4>
+      <h7>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+      </h7>
+    </div>
+  </div>
+</template>
+
+<style lang="less" src="@/styles/desafio.less"></style>
